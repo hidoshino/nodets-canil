b3ae7ad96ad3de0fb061df75bfd5178a807b2e62
hidoshino <83619908+hidoshino@users.noreply.github.com>
2021-08-26 17:09:02 -0300

    Separação das views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,17 @@
 import {Request , Response} from 'express';
 
 export const home = (req: Request, res: Response) => {
-    //res.render('pages/page');
-    res.send("Home no pageController")
+    res.render('pages/page');
 }
 
 export const dogs = (req: Request, res: Response) => {
-
+    res.send('dogs');
 }
 
 export const cats = (req: Request, res: Response) => {
-
+    res.send('Cats');
 }
 
 export const fishes = (req: Request, res: Response) => {
-
+    res.send('Fishes');
 }
